4e1676a9e50e879becf693724762f1f32077d217
Daniel Trejo <daniel.trejo@udem.edu>
2023-10-06 00:11:33 -0600

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -4,3 +4,7 @@ def bubbleSort(arr):
         for j in range(0, n-i-1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            lda = False
+            break
+        
+    print(lda)
